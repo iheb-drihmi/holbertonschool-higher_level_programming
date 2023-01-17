8eb5883f3d9258651acbbc69a1c6dd2580b3ec41
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-01-17 23:21:08 +0100

    4-print_hexa.py

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+for i in range(0, 99):
+    print("{:d} = 0x{:x}".format(i, i))
