e3fb37b0d8bbcc8f24d7b6cdaf6a7833c5b55e34
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-01-20 14:25:12 +0100

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -11,6 +11,8 @@ def add_arg(argv):
             add += int(argv[i])
             i += 1
         print("{:d}".format(add))
+
 if __name__ == "__main__":
-     import sys
-add_arg(sys.argv)
+    import sys
+    add_arg(sys.argv)
+
