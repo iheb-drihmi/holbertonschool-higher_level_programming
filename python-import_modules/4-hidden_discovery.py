3e72a9bba1197498c8fac2aaa4277016b219d52c
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-01-20 14:28:23 +0100

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import hidden_4
 def discovr():
-    name = dir(hidden_4)
-    for i in name:
-        if i[:2] != '__':
-            print("{:s}".format(i))
+name = dir(hidden_4)
+for i in name:
+    if i[:2] != '__':
+        print("{:s}".format(i))
 
 
 if __name__ == "__main__":
