9d48644744863441fdfd1030463fbacf07239df2
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-01-20 14:50:35 +0100

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
-if __name__ == "__main__":
-import sys
-add_arg(sys.argv)
+if __name__ == '__main__':
+    import sys
+    Sum = 0
+    num = sys.argv
+    if len(num) > 1:
+        for i in range(1, len(num)):
+            Sum += int(num[i])
+    print('{:d}'.format(Sum))
