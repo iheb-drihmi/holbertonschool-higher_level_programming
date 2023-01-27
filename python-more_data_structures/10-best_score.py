4207cf446b03b975622897ac5bc9a2a89171ae2e
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-01-27 15:55:30 +0100

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        return (max(a_dictionary.keys()))
+    if not a_dictionary:
+        return None
+    max_key = max(a_dictionary, key=lambda k: a_dictionary[k])
+    return max_key
