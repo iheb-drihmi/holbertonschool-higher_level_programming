2c1ecf6273516f00b267756cc35f2db3c751f0dd
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-01-27 11:58:33 +0100

    1-search_replace.py

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in my_list:
+        if i != search:
+            new_list[len(new_list):] = [i]
+        else:
+            new_list[len(new_list):] = [replace]
+    return new_list
