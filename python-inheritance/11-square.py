c3389a1b8443c39c692955fd80f2db1fb06f9045
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-02-10 02:19:48 +0100

    11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-'''Defines the class Square.'''
+"""
+Defines the class square
+"""
+
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(Rectangle):
-    """Class reps of a rectangle"""
+"""
+Square class
+"""
 
+
+class Square(Rectangle):
+    """ Square Class """
     def __init__(self, size):
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        """ size init"""
         self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
+
 
