8872cfe63914e07f64b443b84215ee6dac694c1c
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-03-17 19:58:00 +0100

    tasks

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" lists all states with a name starting with N (upper N) from
- the database hbtn_0e_0_usa"""
+"""a script that takes in arguments and displays all values in the states"""
 
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
+
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id ASC;""")
+    cur.execute("SELECT * FROM states WHERE name=%s\
+                ORDER BY states.id ASC", (sys.argv[4],))
     i = cur.fetchall()
     for i in i:
         print(i)
